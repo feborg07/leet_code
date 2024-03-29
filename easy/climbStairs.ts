4f5f1aee323449ef441bd3ec58538b3e03a9249d
Felipe Borges <felipe.borges@m2nl.com>
2024-03-29 10:07:32 -0400

    climbStairs

---
+++ easy/climbStairs.ts
@@ -0,0 +1,55 @@
+function climbStairs (n: number): number
+{
+
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    
+    let step1 = 1;
+    let step2 = 2;
+    let curStep = 0
+
+    // It starts at 3 because we handled the first 2 steps already
+    for (let i = 3; i <= n; i++)
+    {
+        curStep = step1 + step2;
+        step1 = step2;
+        step2 = curStep;
+    }
+
+    return curStep;
+}
+
+
+/**
+70. Climbing Stairs
+Easy
+Topics
+Companies
+Hint
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+ */
